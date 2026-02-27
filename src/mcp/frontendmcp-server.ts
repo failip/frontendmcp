2ce299307db441c31ce48bb761a56bda442677fb
Philipp Kuboth <philipp@kuboth.dev>
2026-02-27 22:56:41 +0100

    Server class

---
+++ src/mcp/frontendmcp-server.ts
@@ -0,0 +1,48 @@
+import { type ServerOptions } from "@modelcontextprotocol/sdk/server";
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { type Implementation } from "@modelcontextprotocol/sdk/types.js";
+import { FrontendMCPTransport } from "./frontendmcp-transport";
+
+type FrontendMCPServerConfig = {
+  mcpServerUrl?: string;
+  websocketUrl?: string;
+  uuid?: string;
+  authorizationToken?: string;
+};
+
+export class FrontendMCPServer extends McpServer {
+  readonly authorizationToken: string;
+  readonly url: string;
+  private uuid: string;
+  private websocketUrl: string;
+
+  constructor(serverInfo: Implementation & { frontendMCP?: FrontendMCPServerConfig }, options?: ServerOptions) {
+    super(serverInfo, options);
+
+    const frontendMCPConfig = serverInfo.frontendMCP === undefined ? {
+      mcpServerUrl: "https://frontendmcp.com/mcp",
+      websocketUrl: "wss://frontendmcp.com/mcp",
+    } : serverInfo.frontendMCP;
+
+    frontendMCPConfig.mcpServerUrl = frontendMCPConfig.mcpServerUrl ?? "https://frontendmcp.com/mcp";
+    frontendMCPConfig.websocketUrl = frontendMCPConfig.websocketUrl ?? "wss://frontendmcp.com/mcp";
+
+    frontendMCPConfig.mcpServerUrl = frontendMCPConfig.mcpServerUrl.replace(/\/$/, "");
+    frontendMCPConfig.websocketUrl = frontendMCPConfig.websocketUrl.replace(/\/$/, "");
+
+    frontendMCPConfig.uuid = frontendMCPConfig.uuid ?? crypto.randomUUID();
+    frontendMCPConfig.authorizationToken = frontendMCPConfig.authorizationToken ?? crypto.randomUUID();
+
+    this.uuid = frontendMCPConfig.uuid;
+    this.authorizationToken = frontendMCPConfig.authorizationToken;
+    this.url = `${frontendMCPConfig.mcpServerUrl}/${this.uuid}`;
+    this.websocketUrl = `${frontendMCPConfig.websocketUrl}/${this.uuid}`;
+  }
+
+  public override async connect() {
+    const socket = new WebSocket(this.websocketUrl);
+    const transport = new FrontendMCPTransport(socket, this.authorizationToken);
+    super.connect(transport);
+  }
+
+}
